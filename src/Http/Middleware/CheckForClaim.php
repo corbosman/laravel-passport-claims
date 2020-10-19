f16b84b9c0c4cd05784d059e198d2cb53efc4ac5
Cor Bosman <cor@xs4all.net>
2020-10-19 11:06:40 +0200

    add middleware value check

--- src/Http/Middleware/CheckForClaim.php
+++ src/Http/Middleware/CheckForClaim.php
@@ -11,24 +11,39 @@ class CheckForClaim
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param $claim
      * @return mixed
+     * @throws AuthenticationException
      */
     public function handle($request, Closure $next, $claim)
     {
+        /* check for presence of token */
         if ( ! ($token = $request->bearerToken())) {
             throw new AuthenticationException;
         }
 
+        /* check if token parses properly */
         try {
             $jwt = (new Parser())->parse($token);
         } catch(\Exception $e) {
             throw new AuthenticationException;
         }
 
+        /* check if we want to check both claim and value */
+        [$claim, $value] = explode(',', $claim . ',');
+
         if ($jwt->hasClaim($claim)) {
-            return $next($request);
+
+            if (!$value) {
+                return $next($request);
+            }
+
+            if ($jwt->getClaim($claim) === $value) {
+                return $next($request);
+            }
+
         }
 
         throw new AuthenticationException('Unauthenticated: missing claim');
--- tests/CheckClaimMiddlewareTest.php
+++ tests/CheckClaimMiddlewareTest.php
@@ -46,6 +46,30 @@ class CheckClaimMiddlewareTest extends TestCase
         $this->assertSame('response', $response);
     }
 
+    public function test_request_is_passed_along_if_claim_matches_a_value()
+    {
+        /* set up token with custom claim */
+        $repository = new AccessTokenRepository(m::mock(TokenRepository::class), m::mock(Dispatcher::class));
+        $client = new Client('client-id', 'name', 'redirect');
+        $keys = (new RSA())->createKey(1024);
+        app('config')->set('passport-claims.claims', [MyClaim::class]);
+        $token = $repository->getNewToken($client, [], '');
+        $token->setPrivateKey(new CryptKey($keys['privatekey']));
+        $token->setExpiryDateTime(CarbonImmutable::now()->addHour());
+
+        /* set up request */
+        $request = m::mock();
+        $request->shouldReceive('bearerToken')->andReturn($token->__toString());
+
+        $middleware = new CheckForClaim;
+
+        $response = $middleware->handle($request, function () {
+            return 'response';
+        }, 'my-claim,test');
+
+        $this->assertSame('response', $response);
+    }
+
     public function test_exception_is_thrown_if_token_doesnt_have_claim()
     {
         $this->expectException(AuthenticationException::class);
@@ -67,6 +91,30 @@ class CheckClaimMiddlewareTest extends TestCase
         $response = $middleware->handle($request, function () {
             return 'response';
         }, 'my-claim');
+    }
+
+    public function test_exception_is_thrown_if_claim_does_not_match_value()
+    {
+        $this->expectException(AuthenticationException::class);
+
+        /* set up token with custom claim */
+        $repository = new AccessTokenRepository(m::mock(TokenRepository::class), m::mock(Dispatcher::class));
+        $client = new Client('client-id', 'name', 'redirect');
+        $keys = (new RSA())->createKey(1024);
+        app('config')->set('passport-claims.claims', [MyClaim::class]);
+        $token = $repository->getNewToken($client, [], '');
+        $token->setPrivateKey(new CryptKey($keys['privatekey']));
+        $token->setExpiryDateTime(CarbonImmutable::now()->addHour());
+
+        /* set up request */
+        $request = m::mock();
+        $request->shouldReceive('bearerToken')->andReturn($token->__toString());
+
+        $middleware = new CheckForClaim;
+
+        $response = $middleware->handle($request, function () {
+            return 'response';
+        }, 'my-claim,foo');
     }
 
     public function test_exception_is_thrown_if_request_doesnt_have_token()
