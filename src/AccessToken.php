11fdec9e53505752b6c99924fb5713f850a06150
Vignesh Gurusamy <vignesh@linkstreet.in>
2021-02-03 18:25:27 +0530

    Remove duplicate code using trait

--- src/AccessToken.php
+++ src/AccessToken.php
@@ -3,59 +3,14 @@
 namespace CorBosman\Passport;
 
 use DateTimeImmutable;
-use Lcobucci\JWT\Token;
-use Lcobucci\JWT\Configuration;
 use Illuminate\Pipeline\Pipeline;
-use League\OAuth2\Server\CryptKey;
-use Lcobucci\JWT\Signer\Rsa\Sha256;
-use Lcobucci\JWT\Signer\Key\InMemory;
 use CorBosman\Passport\Traits\ClaimTrait;
-use Lcobucci\JWT\Signer\Key\LocalFileReference;
+use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
 use Laravel\Passport\Bridge\AccessToken as PassportAccessToken;
 
 class AccessToken extends PassportAccessToken
 {
-    use ClaimTrait;
-
-    /**
-     * @var CryptKey
-     */
-    private $privateKey;
-
-    /**
-     * @var Configuration
-     */
-    private $jwtConfiguration;
-
-
-    /**
-     * Generate a string representation from the access token
-     */
-    public function __toString()
-    {
-        return $this->convertToJWT()->toString();
-    }
-
-    /**
-     * Set the private key used to encrypt this access token.
-     * @param CryptKey $privateKey
-     */
-    public function setPrivateKey(CryptKey $privateKey)
-    {
-        $this->privateKey = $privateKey;
-    }
-
-    /**
-     * Initialise the JWT Configuration.
-     */
-    public function initJwtConfiguration()
-    {
-        $this->jwtConfiguration = Configuration::forAsymmetricSigner(
-            new Sha256(),
-            LocalFileReference::file($this->privateKey->getKeyPath(), $this->privateKey->getPassPhrase() ?? ''),
-            InMemory::plainText('')
-        );
-    }
+    use AccessTokenTrait, ClaimTrait;
 
     /**
      * Generate a JWT from the access token
